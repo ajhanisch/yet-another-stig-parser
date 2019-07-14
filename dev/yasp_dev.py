761cb834e311e6c8726a88a7974e7236738ceada
Ashton Hanisch <ajhanisch@gmail.com>
2019-07-14 16:35:05 -0400

    Update yasp_dev.py

--- dev/yasp_dev.py
+++ dev/yasp_dev.py
@@ -31,70 +31,30 @@ class Setup:
     '''
     required = parser.add_argument_group('Required', 'Ensure to use all of these parameters to run [{}] successfully.'.format(program))
     required.add_argument(
-    '--input',
-    metavar='[dir | .csv]',
-    type=str,
-    help='Directory that contains the .csv file(s) exported from STIG Viewer results or an individual .csv file.'
+        '--input',
+        metavar='[dir | .csv]',
+        type=str,
+        help='Directory that contains the .csv file(s) exported from STIG Viewer results or an individual .csv file.'
     )
 
     '''
     OPTIONAL ARGUMENTS
     '''
     parser.add_argument(
-        '--dashboard',
+        '--output',
         choices=['excel'],
         default='excel',
-        help='Create summary document from parsed files.'
-    )
-
-    parser.add_argument(
-    '--host',
-    metavar='[host]',
-    type=str,
-    help='Print a list of findings associated with a specific host.'
-    )
-
-    parser.add_argument(
-    '--hosts',
-    choices=['full', 'basic'],
-    help='Print a list of hosts parsed. Full includes: (ip address, host name, and mac address.). Basic includes: (ip address and host name).'
-    )
-
-    parser.add_argument(
-    '--raw',
-    action='store_true',
-    default=False,
-    help='Print parsed information in raw mode (debug).'
-    )
-
-    parser.add_argument(
-    '--stats',
-    action='store_true',
-    default=False,
-    help='Print statistisc about parsed files.'
-    )
-
-    parser.add_argument(
-    '--verbose',
-    choices=[ 'debug', 'info', 'warning', 'error', 'critical' ],
-    default='info',
-    help='Enable specific program verbosity. Default is info. Set to debug for complete script processing in logs and screen. Set to warning or critical for minimal script processing in logs and screen.'
-    )
-
-    parser.add_argument(
-    '--vulnid',
-    metavar='[vulnid]',
-    type=str,
-    help='Print a list of hosts vulnerable to specific STIG vulnerability ID.'
+        help='output summary from parsed files.'
     )
 
     '''
     VERSION
     '''
     parser.add_argument(
-    '--version',
-    action='version',
-    version='[{}] - Version [{}]. Check [{}] for the most up to date information.'.format(program, version, repository)
+        '--version',
+        '-v',
+        action='version',
+        version='{}'.format(version)
     )
 
     args = parser.parse_args()
@@ -125,7 +85,7 @@ class StigParser:
 
     def __init__(self, parser_input):
         if parser_input == None or parser_input == '':
-            logging.error('[!] No filename specified!')
+            print('No filename specified!')
             exit()
 
         # Parse input values in order to find valid .csv files
@@ -141,12 +101,12 @@ class StigParser:
                 break
         elif parser_input.endswith('.csv'):
             if not exists(parser_input):
-                logging.error('[!] File [{}] does not exist.'.format(parser_input))
+                print('File [{}] does not exist.'.format(parser_input))
                 exit()
             self.list_csv.append(parser_input)
 
         if not self.list_csv:
-            logging.error('[!] No file [{}] to parse was found!'.format(parser_input))
+            print('No file [{}] to parse was found!'.format(parser_input))
             exit()
 
         # dictionary to store results
@@ -164,7 +124,6 @@ class StigParser:
             if category not in self._results:
                 self._results[category] = {}
 
-            #stig = sub('[^A-Za-z0-9]+', ' ', row['STIG'])
             stig = row['STIG']
             if stig not in self._results[category]:
                 self._results[category][stig] = []
@@ -194,9 +153,32 @@ class StigParser:
     def create_stig_poams_dashboard_excel(self):
         workbook = Workbook('hello.xlsx')
 
+        '''
+        Headers used by Category and Completed POA&M tabs.
+        '''
+        headers = [
+            'HostName',
+            'IPAddress',
+            'Vuln ID',
+            'Status',
+            'Severity',
+            'Rule Title',
+            'Comments',
+            'STIG',
+            'Security Control',
+            'POC',
+            'Resources Required',
+            'Scheduled Completion Date',
+            'Milestones with Completion Dates',
+            'Changes to Milestones',
+            'Identified By',
+            'Estimated Cost',
+        ]
+
         self.create_stig_poams_dashboard_excel_dashboard_tab(workbook=workbook)
-        self.create_stig_poams_dashboard_excel_category_tab(workbook=workbook)
+        self.create_stig_poams_dashboard_excel_category_tab(workbook=workbook, headers=headers)
         self.create_stig_poams_dashboard_excel_details_tab(workbook=workbook)
+        self.create_stig_poams_dashboard_excel_completed_poams_tab(workbook=workbook, headers=headers)
 
         workbook.close()
 
@@ -269,42 +251,20 @@ class StigParser:
             }
         )
 
-        format_total = workbook.add_format(
-            {
-                'bottom' : 1,
-                'top' : 1,
-                'bold' : 1,
-                'font_color' : 'black',
-                'align' : 'center',
-                'valign' : 'vcenter'
-            }
-        )
-
         '''
         Create each overview section.
         '''
-        worksheet.merge_range('A1:E1', 'Category Overview', format_merged_header)
+        worksheet.merge_range('A1:D1', 'Category Overview', format_merged_header)
         worksheet.write('A3', 'Category', format_overview_header)
         worksheet.write('B3', 'CAT I', format_cat_i)
         worksheet.write('C3', 'CAT II', format_cat_ii)
         worksheet.write('D3', 'CAT III', format_cat_iii)
-        worksheet.write('E3', 'Total', format_overview_header)
-
-        worksheet.merge_range('G1:K1', 'STIG Overview', format_merged_header)
-        worksheet.write('G3', 'STIG', format_overview_header)  
-        worksheet.write('H3', 'CAT I', format_cat_i)
-        worksheet.write('I3', 'CAT II', format_cat_ii)
-        worksheet.write('J3', 'CAT III', format_cat_iii)
-        worksheet.write('K3', 'Total', format_overview_header)
 
         '''
         Set each overview row and column numbers.
         '''
         row_category_overview = 3
         column_category_overview = 0
-
-        row_stig_overview = 3
-        column_stig_overview = 6
 
         '''
         Populate category overview cells.
@@ -317,39 +277,25 @@ class StigParser:
             worksheet.write_url(row_category_overview, column_category_overview, "internal:'{}'!A1".format(category), format_overview_values, string=category)
 
             for stig_name in self._results[category].keys():
-                worksheet.write(row_stig_overview, column_stig_overview, stig_name.split('Security Technical Implementation Guide')[0], format_overview_values)
-
                 count_stig_cat_i = len(self.find_by_values(category=category, stig=stig_name, values=['Open', 'high']))
                 count_stig_cat_ii = len(self.find_by_values(category=category, stig=stig_name, values=['Open', 'medium']))
                 count_stig_cat_iii = len(self.find_by_values(category=category, stig=stig_name, values=['Open', 'low']))
 
                 count_category_cat_i += count_stig_cat_i
                 count_category_cat_ii += count_stig_cat_ii
-                count_category_cat_iii += count_stig_cat_iii
-
-                '''
-                Write stig overview values.
-                '''
-                count_stig_total = count_stig_cat_i + count_stig_cat_ii + count_stig_cat_iii
-                worksheet.write(row_stig_overview, column_stig_overview + 1, count_stig_cat_i, format_cat_i)
-                worksheet.write(row_stig_overview, column_stig_overview + 2, count_stig_cat_ii, format_cat_ii)
-                worksheet.write(row_stig_overview, column_stig_overview + 3, count_stig_cat_iii, format_cat_iii)
-                worksheet.write(row_stig_overview, column_stig_overview + 4, count_stig_total, format_total)         
-                row_stig_overview += 1       
-                
+                count_category_cat_iii += count_stig_cat_iii              
 
             '''
             Write category overview values.
             '''
             count_category_total = count_category_cat_i + count_category_cat_ii + count_category_cat_iii
-            worksheet.write(row_category_overview, column_category_overview + 1, count_category_cat_i, format_cat_i)
-            worksheet.write(row_category_overview, column_category_overview + 2, count_category_cat_ii, format_cat_ii)
-            worksheet.write(row_category_overview, column_category_overview + 3, count_category_cat_iii, format_cat_iii)
-            worksheet.write(row_category_overview, column_category_overview + 4, count_category_total, format_total)
 
+            worksheet.write_formula(row_category_overview, column_category_overview + 1, "=COUNTIF('{}'!E15:E{}, \"{}\")".format(category, count_category_total + 15, "high"), format_cat_i)
+            worksheet.write_formula(row_category_overview, column_category_overview + 2, "=COUNTIF('{}'!E15:E{}, \"{}\")".format(category, count_category_total + 15, "medium"), format_cat_ii)
+            worksheet.write_formula(row_category_overview, column_category_overview + 3, "=COUNTIF('{}'!E15:E{}, \"{}\")".format(category, count_category_total + 15, "low"), format_cat_iii)
             row_category_overview += 1
 
-    def create_stig_poams_dashboard_excel_category_tab(self, workbook):
+    def create_stig_poams_dashboard_excel_category_tab(self, workbook, headers):
         for category in self._results.keys():
             '''
             Create category worksheet.
@@ -458,26 +404,7 @@ class StigParser:
 
             '''
             Write headers.
-            '''
-            headers = [
-                'HostName',
-                'IPAddress',
-                'Vuln ID',
-                'Status',
-                'Severity',
-                'Rule Title',
-                'Comments',
-                'STIG',
-                'Security Control',
-                'POC',
-                'Resources Required',
-                'Scheduled Completion Date',
-                'Milestones with Completion Dates',
-                'Changes to Milestones',
-                'Identified By',
-                'Estimated Cost',
-            ]
-            
+            '''            
             worksheet.write_row(row, col, headers, format_finding_headers)
             worksheet.autofilter(row, col, row, len(headers) - 1)
             row += 1
@@ -555,6 +482,96 @@ class StigParser:
                             worksheet.autofilter(row, col, row, len(finding.keys()) - 1)
                             row += 1
 
+    def create_stig_poams_dashboard_excel_completed_poams_tab(self, workbook, headers):
+        '''
+        Create complete POA&M worksheet.
+        '''
+        worksheet = workbook.add_worksheet('Completed POA&Ms')
+
+        '''
+        Set worksheet row and column values to properly populate values.
+        '''
+        row = 0
+        col = 0
+
+        '''
+        Formatting settings.
+        '''
+        format_finding_headers = workbook.add_format(
+            {
+                'bold' : 1,
+                'border' : 1,
+                'align' : 'center',
+                'valign' : 'vcenter',
+                'fg_color' : '#87CEFA'
+            }
+        )
+
+        format_finding_values = workbook.add_format(
+            {
+                'border' : 1,
+                'align' : 'left'
+            }
+        )
+
+        format_boilerplate_titles = workbook.add_format(
+            {
+                'bold' : 1,
+                'border' : 1,
+                'align' : 'center'
+            }
+        )
+
+        format_boilerplate_headers = workbook.add_format(
+            {
+                'bold' : 1,
+                'border' : 1,
+                'align' : 'right',
+                'fg_color' : '#87CEFA'
+            }
+        )
+
+        format_boilerplate_values = workbook.add_format(
+            {
+                'border' : 1,
+                'align' : 'right',
+                'fg_color' : '#C0C0C0'
+            }
+        )
+
+        '''
+        Populate boiler plate information.
+        '''
+        worksheet.merge_range(row, col, row, 1, 'Completed POA&M Items', format_boilerplate_titles)
+        row += 1
+
+        worksheet.write(row, col, 'System Name : ', format_boilerplate_headers)
+        worksheet.write(row, col + 1, '', format_boilerplate_values)
+        row += 1
+
+        worksheet.write(row, col, 'Company/Organization Name : ', format_boilerplate_headers)
+        worksheet.write(row, col + 1, '', format_boilerplate_values)
+        row += 1
+
+        worksheet.write(row, col, 'Sponsoring Service/Agency : ', format_boilerplate_headers)
+        worksheet.write(row, col + 1, '', format_boilerplate_values)
+        row += 2
+
+        '''
+        Write headers.
+        '''            
+        worksheet.write_row(row, col, headers, format_finding_headers)
+        worksheet.autofilter(row, col, row, len(headers) - 1)
+        row += 1
+
+        '''
+        Populate example values.
+        '''
+        list_blank_values = [ '' for header in headers ]
+        for i in range(5):
+            worksheet.write_row(row, col, list_blank_values, format_finding_values)
+            row += 1
+
 def main():
     '''
     MAIN FUNCTION
@@ -563,7 +580,7 @@ def main():
     args = setup.args
 
     if not args.input:
-        logging.error('[!] No operation specified!')
+        print('No operation specified!')
         exit()
 
     '''
@@ -573,44 +590,13 @@ def main():
         if not exists(value):
             makedirs(value)
 
-    '''
-    SETUP LOGGING
-    '''
-    dict_levels = {
-    'debug': logging.DEBUG,
-    'info': logging.INFO,
-    'warning': logging.WARNING,
-    'error': logging.ERROR,
-    'critical': logging.CRITICAL,
-    }
-
-    level_name = args.verbose
-    level = dict_levels.get(level_name)
-    format = '[%(asctime)s] - [%(levelname)s] - %(message)s'
-    handlers = [logging.FileHandler(setup.file_log), logging.StreamHandler()]
-    logging.basicConfig(
-        level = level,
-        format = format,
-        handlers = handlers
-    )
-
     parser = StigParser(args.input)
 
     '''
     ARGUMENT HANDLING
     '''
-    if args.hosts:
-        parser.print_hosts(fullinfo=args.hosts)
-    if args.vulnid:
-        parser.find_by_vulnid(vulnid=args.vulnid, output='print')
-    if args.host:
-        parser.find_by_host(host=args.host)
-    if args.raw:
-        parser.print_raw()
-    if args.stats:
-        parser.print_statistics()
-    if args.dashboard:
-        parser.create_stig_poams_dashboard(format_option=args.dashboard)
+    if args.output:
+        parser.create_stig_poams_dashboard(format_option=args.output)
 
     exit()
 
